e18e5d93332134ceea83147e8a0631492b567bf4
Alexander Cherepnya <xitman78@me.com>
2017-02-10 22:01:43 +0200

    CombineReducers for Modals

--- src/reducers/modals.js
+++ src/reducers/modals.js
@@ -1,39 +1,47 @@
-export default function modals(state = {}, action) {
+import { combineReducers } from 'redux'
+
+function alert(state = {}, action) {
 
   switch(action.type) {
+
     case 'MODAL_ALERT_FIRED':
       return {
-        ...state,
-        alert: {
           open: true,
           ...action.payload,
-        } };
+        };
     case 'MODAL_ALERT_CLOSED':
       return {
-        ...state,
-        alert: {
           open: false
-        }
-      };
-
-    case 'MODAL_SNACKBAR_FIRED':
-      return {
-        ...state,
-        snackbar: {
-          open: true,
-          ...action.payload,
-        } };
-
-    case 'MODAL_SNACKBAR_CLOSED':
-      return {
-        ...state,
-        snackbar: {
-          open: false
-        }
       };
 
     default:
       return state;
+      
   }
-
 }
+
+function snackbar(state = {}, action) {
+
+  switch(action.type) {
+
+  case 'MODAL_SNACKBAR_FIRED':
+    return {
+        open: true,
+        ...action.payload,
+      };
+
+  case 'MODAL_SNACKBAR_CLOSED':
+    return {
+        open: false
+    };
+
+  default:
+    return state;
+
+  }
+}
+
+export default combineReducers({
+  alert,
+  snackbar
+});
