2a45d452610e58a5b4c60061d0f785e7cf549c4e
Alexander Cherepnya <xitman78@gmail.com>
2017-03-02 18:10:53 +0200

    Splat preset

--- src/animations/bounce.js
+++ src/animations/bounce.js
@@ -81,8 +81,62 @@ smack.skew({
   stiffness: 2
 });
 
+let swoosh = new Bounce();
+
+swoosh.title = "Swoosh";
+
+swoosh.translate({
+  from: { x: 0, y: 0 },
+  to: { x: 300, y: 0 },
+  easing: 'bounce',
+  duration: 1000,
+  delay: 0,
+  bounces: 4,
+  stiffness: 5
+}).scale({
+  from: { x: 1, y: 1 },
+  to: { x: 15, y: 1 },
+  easing: 'sway',
+  duration: 500,
+  delay: 0,
+  bounces: 4,
+  stiffness: 5
+});
+
+let splat = new Bounce();
+
+splat.title = "Splat";
+
+splat.translate({
+  from: { x: 0, y: 0 },
+  to: { x: 300, y: 0 },
+  easing: 'bounce',
+  duration: 600,
+  delay: 0,
+  bounces: 4,
+  stiffness: 4
+}).scale({
+  from: { x: 1, y: 1 },
+  to: { x: 0.1, y: 2.3 },
+  easing: 'sway',
+  duration: 800,
+  delay: 65,
+  bounces: 4,
+  stiffness: 2
+}).scale({
+  from: { x: 1, y: 1 },
+  to: { x: 5, y: 1 },
+  easing: 'sway',
+  duration: 300,
+  delay: 30,
+  bounces: 4,
+  stiffness: 3
+});
+
 export default {
   foldUnfold,
   jelly,
-  smack
+  smack,
+  splat,
+  swoosh,
 };
